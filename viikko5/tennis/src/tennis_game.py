ad36c10963e3202bcb226852124a12d05dd1e72a
mjouppil <markkujouppila@outlook.com>
2023-12-02 11:54:34 +0200

    Viikko 5 - tennis - commit6

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -20,33 +20,18 @@ class TennisGame:
             score = self._get_odd_score(self.m_score1, self.m_score2)
         return score
 
+    def _get_point_names(self, m_score):
+        point_names = ["Love", "Fifteen", "Thirty", "Forty"]
+        return point_names[m_score]
+
     def _get_even_score(self, m_score):
-        if m_score < 3:
-            score = "".join([self._get_point_names(m_score), "-All"])
-        else:
-            score = "Deuce"
-        return score
+        return "-".join([self._get_point_names(m_score), "All"]) if m_score < 3 else "Deuce"
+
+    def _get_odd_score(self, m_score1, m_score2):
+        return "-".join([self._get_point_names(m_score1), self._get_point_names(m_score2)])
 
     def _get_endgame_score(self, m_score1, m_score2):
         difference = m_score1 - m_score2
         state = "Advantage" if abs(difference) <= 1 else "Win for"
         player = "player1" if difference > 0 else "player2"
-        score = " ".join([state, player])
-        return score
-
-    def _get_odd_score(self, m_score1, m_score2):
-        score = ""
-        temp_score = 0
-        for i in range(1, 3):
-            if i == 1:
-                temp_score = m_score1
-            else:
-                score = score + "-"
-                temp_score = m_score2
-            score = score + self._get_point_names(temp_score)
-        return score
-
-    def _get_point_names(self, m_score):
-        point_names = ["Love", "Fifteen", "Thirty", "Forty"]
-        score = point_names[m_score]
-        return score
+        return " ".join([state, player])
